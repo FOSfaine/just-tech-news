cc03d79abab63a20fb4fa2e4d209d09c580e5dcb
Esther Cho <eunsem.64642@gmail.com>
2022-02-21 12:50:35 -0700

    Comment.js in models and commnent-route.js in api/routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -17,22 +17,30 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+        attributes: { exclude: ['password'] },
         where: {
           id: req.params.id
         },
-        attributes: { exclude: ['password'] },
-        // replace the existing `include` with this
         include: [
-            {
+          {
             model: Post,
             attributes: ['id', 'title', 'post_url', 'created_at']
-            },
-            {
+          },
+          // include the Comment model here:
+          {
+            model: Comment,
+            attributes: ['id', 'comment_text', 'created_at'],
+            include: {
+              model: Post,
+              attributes: ['title']
+            }
+          },
+          {
             model: Post,
             attributes: ['title'],
             through: Vote,
             as: 'voted_posts'
-            }
+          }
         ]
       })
         .then(dbUserData => {

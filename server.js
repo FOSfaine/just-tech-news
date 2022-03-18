f8cb48acf58b8c9ce9cc8073806ab0b9a8d4cd0c
Esther Cho <eunsem.64642@gmail.com>
2022-03-17 19:42:36 -0600

    Login, logout, auth and sessions added

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -19,6 +20,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+	secret: 'Super secret secret',
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+	}),
+};
+
+app.use(session(sess));
 
 // turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {

facd8c5f8343622212b1b0a5d3d8817effdce479
Esther Cho <eunsem.64642@gmail.com>
2022-03-21 15:36:31 -0600

    dashboard edit and delte post functions, addAuth added

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', (req, res) => {});
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
 	// check the session
 	if (req.session) {
 		Comment.create({
@@ -20,6 +21,6 @@ router.post('/', (req, res) => {
 	}
 });
 
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', withAuth, (req, res) => {});
 
 module.exports = router;
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote } = require('../../models');
+const withAuth = require('../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
@@ -139,7 +140,7 @@ router.put('/:id', (req, res) => {
 });
 
 // DELETE /api/users/1
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
 	User.destroy({
 		where: {
 			id: req.params.id,
